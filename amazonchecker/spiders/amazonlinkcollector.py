ff95ea8b24934ea9ebfea35c54469479335d741f
Art Morozov <artyom.morozov315@gmail.com>
2023-01-14 19:37:24 -0500

    Add ID to spider on creation

--- amazonchecker/spiders/amazonlinkcollector.py
+++ amazonchecker/spiders/amazonlinkcollector.py
@@ -1,6 +1,7 @@
 from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider, Spider
+from scrapy import signals
 from urllib.parse import urlparse, parse_qs, urlencode
 from amazonchecker.items import AmazonProductItem, FailedLinkItem
 import re
@@ -8,7 +9,6 @@ import re
 class AmazonLinkCollector(Spider):
     name = 'amazon_spider'
     handle_httpstatus_list = [400, 403, 404, 406, 409, 410, 500, 501, 502, 503]
-
 
     def get_url(self, url):
         if not self.settings.get("SCRAPPER_API"):
@@ -25,46 +25,34 @@ class AmazonLinkCollector(Spider):
     def is_invalid_amazon(self, amazon_url):
         return "amazon" in amazon_url and any([deny in amazon_url for deny in ('review', "register", "signin", "/customer-preferences/", "forgot-password", "openid", "kdp.amazon", "aws.amazon", "your-account", "prime", "/gp/help/customer/")])
 
-    def __init__(self, start_url=None, *args, **kwargs):
+    def __init__(self, start_url=None, spider_id=0, *args, **kwargs):
         super(AmazonLinkCollector, self).__init__(*args, **kwargs)
-        self.start_urls = [start_url] if start_url is not None else ["https://streammentor.com/"]
+        self.id = spider_id
+        self.start_urls = [start_url] if start_url is not None else []
         parts = urlparse(start_url)
         self.allowed_domains = ["api.scraperapi.com", "amazon.com", "merch.amazon.com", parts.netloc]
         self.asin_patten = re.compile('.*\/([a-zA-Z0-9]{10})(?:[/?]|$).*')
         self.amazon_extractor = LinkExtractor(deny=(r'review', r'openid', r"register", r"signin", r"\/customer-preferences\/", r"forgot-password", r"\/your-account", r"prime", r"\/gp\/help\/customer\/"),allow_domains=["amazon.com", "amazon.to", "amzn.to", "amzn", "amzn.com"], deny_domains=["merch.amazon.com", "affiliate-program.amazon.com","kdp.amazon.com", "aws.amazon.com"])
-        self.regular_extractor = LinkExtractor(deny=(r'\/wp-admin\/'), allow_domains=[parts.netloc], deny_extensions=("webp", "jpg", "png", "gif") )
+        self.regular_extractor = LinkExtractor(deny=(r'\/wp-admin\/'), allow_domains=[parts.netloc], deny_extensions=("webp", "jpg", "png", "gif") )       
+        self.logger.info(f"Amazon Spider with id {self.id} created for url {start_url} with allowed domain {parts.netloc}")
 
-        # self.rules = (
-        #     # Amazon Link Extractor
-        #     Rule(LinkExtractor(deny=(r'review', r'openid', r"register", r"signin", r"\/customer-preferences\/", r"forgot-password", ),allow_domains=["merch.amazon.com", "amazon.com", "amazon.to", "amzn.to", "amzn", "amzn.com"], deny_domains=["kdp.amazon.com", "aws.amazon.com"]),
-        #     callback='parse_amazon'),
-
-        #     # Extract regular links
-        #     Rule(LinkExtractor(allow_domains=[parts.netloc]), callback='parse_regular'),
-        # )
-        self.logger.info(f"Amazon Spider created with for url {start_url} with allowed domain {parts.netloc}")
-
+    # check if link belongs to the same domain
     def is_site_link(self, url):
         return self.start_urls[-1] in url
 
+    # Check if link is correctly cloaked
     def is_icorrectly_cloaked(self, response):
         if not self.is_site_link(response.url):
             return False
         if "amazon.com" in response.xpath('//title/text()').extract_first().lower():
-            print("title - ",response.xpath('//title/text()').extract_first().lower())
-            print('Url - ', response.url)
             return True
         return False
-
-
-
-
 
     def parse(self, response):
         self.logger.info('Parse function called on %s', response.url)
 
         if self.is_invalid_amazon(response.url):
-            return None
+            return
 
         # Check if the link is broken
         if response.status in range(400, 600):
@@ -74,7 +62,7 @@ class AmazonLinkCollector(Spider):
             item['response']= response.url
             item['referer'] = response.request.headers.get('Referer', "")
             yield item
-            return None
+            return
         if self.is_icorrectly_cloaked(response):
             self.logger.error(f"Icorrectly Cloaked Link, saving\n\n")
             item = FailedLinkItem()
@@ -82,28 +70,17 @@ class AmazonLinkCollector(Spider):
             item['response']= response.url
             item['referer'] = response.request.headers.get('Referer', "")
             yield item
-            return None
+            return
 
-        self.logger.error(f"\n\n Link history for {response.url} {response.request.meta.get('redirect_urls')}\n\n")
-
-        # redirect_history = response.request.meta.get('redirect_urls', [])
-
-        # all_links = list(filter(self.is_amazon_product_page, redirect_history))
-
-        # if redirect_history and all_links:
-        #     amazon_link = all_links[0]
-        #     self.logger.warning('AMAZON LINK WAS THERE %s but now its not :( %s\n\n', amazon_link, response.url)
-        #     yield Request(url=self.get_url(amazon_link), callback=self.parse_amazon)
-        #     return None
-        
+        self.logger.error(f"\n\n Link history for {response.url} {response.request.meta.get('redirect_urls')}\n\n")      
 
         # Check if the link redirects to Amazon.com
         if self.is_amazon_product_page(response.url):
             self.logger.warning('Current link is AMAZON, calling parse_amazon function... %s\n\n', response.url)
             yield self.parse_amazon(response)
-            return None
+            return
 
-        # return None
+        # return
         #  Left From Regular Spider
         if self.is_site_link(response.url):
             self.logger.warning('Url is site link, collecting links to crawl %s\n\n', response.url)
@@ -129,10 +106,8 @@ class AmazonLinkCollector(Spider):
                 # yield Request(url=self.get_url(lnk.url), callback=self.parse_amazon)
                 yield response.follow(lnk.url, callback=self.parse)
         else:
-            return None
+            return
         
-    
-    
     def parse_amazon(self, response):
         amazon_url = response.meta.get("orig_url", None)
         if not amazon_url:
@@ -186,42 +161,4 @@ class AmazonLinkCollector(Spider):
             item["bullet_points"] = response.xpath('//*[@id="feature-bullets"]//li/span/text()').extract()
             item["seller_rank"] = response.xpath('//*[text()="Amazon Best Sellers Rank:"]/parent::*//text()[not(parent::style)]').extract()
             
-            yield item
-
-
-
-    # def parse_regular(self, response):
-    #     self.logger.info('Parse function called on %s', response.url)
-
-    #     # Check if the link is broken
-    #     if response.status in range(400, 600):
-    #         self.logger.error(f"\n\nFAILING status code in regular link\n\n")
-    #         item = AmazonProductItem()
-    #         item['referer'] = str(response.request.headers.get('Referer', None))
-    #         item['status'] = response.status
-    #         item['response']= response.url
-    #         return item
-
-    #     redirect_statuses = (301, 302, 303, 307, 308)
-        
-
-    #     redirect_urls = response.request.meta.get('redirect_urls', None)
-
-
-    #     # Hanlde Cloaked Amazon Links
-    #     if redirect_urls is not None: # <== this is condition of redirection
-    #         self.logger.info("\n\n\nWe have to REDIRECTED, current url-- %s\n\n\n", response.url)
-    #         first_amazon_link = next((u for u in redirect_urls if self.is_amazon_product_page(u)), None)
-    #         if first_amazon_link is not None:
-    #             # Got Amazon 
-    #             self.logger.info("\n\n\nFound AMAZON in redirects, following...\n\n\n")
-    #             return self.parse_amazon(response)
-                
-
-    #     # Check if the link redirects to Amazon.com
-    #     if self.is_amazon_product_page(response.url):
-    #         self.logger.warning('AMAZON LINK %s\n\n', response.url)
-    #         return self.parse_amazon(response)
-
-
-    
+            yield item    
